3bb3f92dc2693e08025eda90e43246ed8cfa6144
Isaac Thill <ithill22@turing.edu>
2023-02-07 21:56:35 -0700

    Add iterations 1-4 solutions

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,37 @@
 class Venue
+
+  attr_reader :name, :capacity
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def patrons
+    @patrons
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map{|patron|patron.upcase} # shorthand for below
+    # # @patrons.map do |patron|
+    #   patron.upcase
+  end
+
+  def over_capacity?
+    @patrons.count > @capacity
+  end
+
+  def kick_out
+    if over_capacity?
+      @patrons.pop(@patrons.count - @capacity)
+    end
+  end
+
 end
+
+
+# lambda { |x| x.upcase }
